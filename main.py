48b0dd877fa0b1f2660ce0e68f7c04e435f53f78
Denis <denis.kazancev-2000@yandex.ru>
2022-04-11 22:43:05 +0300

    First work version

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
 import numpy as np
 import cv2 as cv
 
+
 def mainFunc():
-    img = cv.imread('image2.jpg') #36
-    output = img.copy()
+    img = cv.imread('wheelOnBlack.jpg')  # 36
 
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    blurred = cv.medianBlur(gray, 51)
-    T, thresh_img = cv.threshold(blurred, 240, 255, cv.THRESH_BINARY)
+    blurred = cv.medianBlur(gray, 3)
+    kernel = np.ones((2, 2), 'uint8')
+    # T, thresh_img = cv.threshold(blurred, 240, 255, cv.THRESH_BINARY)
 
-    rows = blurred.shape[0]
-    cv.imshow('img', thresh_img)
-    cv.waitKey(0)
-    circles = cv.HoughCircles(thresh_img, cv.HOUGH_GRADIENT, 1, rows / 8,
-                              param1=100, param2=30,
-                              minRadius=1, maxRadius=30)
+    erode_img = cv.erode(gray, kernel, cv.BORDER_REFLECT, iterations=1)
+
+    circles = cv.HoughCircles(erode_img , cv.HOUGH_GRADIENT, 1.4, 25,param1=350,param2=55,maxRadius=75,minRadius=25)
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
-            center = (i[0], i[1])
+            center = (i[1] + 10, i[0] + 5)
             # circle center
-            cv.circle(img, center, 1, (0, 100, 100), cv.FILLED, 3)
+            cv.circle(erode_img, center, 1, (0, 100, 100), 3)
             # circle outline
-            radius = i[2]
-            cv.circle(img, center, radius, (255, 255, 255), cv.FILLED, 3)
+            radius = i[2] + 245
+            cv.circle(erode_img, center, radius, (0, 0, 0), cv.FILLED, 3)
 
-    cv.imshow('img', img)
+    corners = cv.goodFeaturesToTrack(erode_img, 70, 0.1, 20)  #0.5 works
+
+    for corner in corners:
+        x, y = corner.ravel()
+        cv.circle(img, (int(x), int(y)), 5, (0, 255, 0), -1)
+
+    print("count angle", len(corners))
+
+    cv.imshow('thresh_img', img)
     cv.waitKey(0)
-
-    # corners = cv.goodFeaturesToTrack(gray, 70, 0.5, 20) #0.5 works
-    #
-    # for corner in corners:
-    #     x, y = corner.ravel()
-    #     cv.circle(img, (int(x), int(y)), 5, (36, 255, 12), -1)
-    #
-    # print("count angle", len(corners))
-    #
-    # cv.imshow('thresh_img', img)
-    # cv.waitKey(0)
 
     cv.destroyAllWindows()
 
